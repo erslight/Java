cdd328011caac2d9096bcfffeffe69870b837f17
Zhivolkovskaya Liza <zhivolkovskaya.liza@yandex.ru>
2019-06-07 23:47:02 +0300

    Krasota

--- src/Cats/Main.java
+++ src/Cats/Main.java
@@ -1,23 +1,25 @@
 package Cats;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class Main {
-
     public static void main(String[] args){
 
-        Cat cats = new Cat();       // создали cats (кота) из класса Cat
-        System.out.println(cats.toString());
 
 
-        ArrayList<String> cat = new ArrayList<String>();   //cat - это коллекция
+
+        ArrayList<Cat> catList = new ArrayList<>();   //catList - это коллекция
         for(int i = 0; i<40; i++) {
-            cat.add("Kot"+(i+1));
-            System.out.println(cat.get(i));
+            Cat cats = new Cat();       // создали cats (кота) из класса Cat
+            System.out.println(cats.toString());
+            catList.add(cats);
+            System.out.println(catList.get(i));
         }
-        for(int i = 0; i<cat.size(); i++) {
+        for(int i = 0; i<catList.size(); i++) {
             if(i%2==0){
-                cat.get(i).setColor = Colorcat.BLACK;
+                catList.get(i).setColor(Colorcat.BLACK);
+                System.out.println(catList.get(i));
             }
         }
     }
